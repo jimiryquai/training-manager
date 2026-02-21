f87d4c88f7532a518a8d74aaca19f1d675fbefeb
JamesManc1tyFC!^ <james@jamesryan.dev>
2026-02-21 13:57:59 +0000

    feat: configure Tailwind CSS v4

--- src/app/document.tsx
+++ src/app/document.tsx
@@ -5,8 +5,9 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>@redwoodjs/starter-minimal</title>
+      <title>Training Manager</title>
       <link rel="modulepreload" href="/src/client.tsx" />
+      <link rel="stylesheet" href="/src/app/styles/globals.css" />
     </head>
     <body>
       {children}
